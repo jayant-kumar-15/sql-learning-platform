339f9d118880291ac9f04e58a3018cc23e523dac
Jayant Kumar <79749366+jayant-kumar-15@users.noreply.github.com>
2026-08-09 20:34:09 +0530

    Use environment variable for server port

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -5,7 +5,7 @@ const queryRoutes = require("../routes/queryRoutes");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api", queryRoutes);
